611fbf01a5a728e8e8549a382d29ff75e8532429
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-06-21 19:28:29 -0400

    orm.js update method added

--- config/orm.js
+++ config/orm.js
@@ -8,5 +8,14 @@ var orm =   {
             // this is referring to our call back function
             cb(result)
         })
-    }
+    },
+    // this is our update method that allows us to udpate our table, whether it is devoured and references our call back
+    update: function (tableInput, condition, cb){
+    connection.query('UPDATE' + tableInput + ' SET devoured=true WHERE id='+condition+';', function(err, result) {
+        if(err) throw err;
+        cb(result);
+
+})
 }
+}
+module.exports = orm;
