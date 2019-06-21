16bb29b9ee5797f313266ea241f01c268065c737
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-06-21 19:22:00 -0400

    index.hanldebars updated!

--- config/orm.js
+++ config/orm.js
@@ -2,7 +2,7 @@ var connection = require('./connection.js');
 // this will collect data from our database
 var orm =   {
     all: function (tableInput, cb)  {
-        connection.query('SELECT * FROM ' + tableInput + ';' function(err,
+        connection.query('SELECT * FROM ' + tableInput + ';', function(err,
         result){
             if(err) throw err;
             // this is referring to our call back function
