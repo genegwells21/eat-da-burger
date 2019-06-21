1a4b9613c17d81d68fe25b403fa0a4842c0644ac
genegwells21 <46447933+genegwells21@users.noreply.github.com>
2019-06-21 18:11:57 -0400

    routes.js in controllers added

---
+++ controllers/routes.js
@@ -0,0 +1,6 @@
+var express = router('express');
+var router = express.Router();
+// this is a setting up our path with a get function
+router.get('/', function(req, res)  {
+    res.render('index');
+})
